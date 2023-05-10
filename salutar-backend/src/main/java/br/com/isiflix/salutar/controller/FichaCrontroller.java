354bf310e3df2b4c71f1f84bcadefebde8a06981
Jefferson Pedro <jeffersonpedro05@gmail.com>
2023-05-09 22:48:30 -0300

    update
    Criação dos métodos para CRUD na classe FichaController:
    - cadastrarNovaFicha;
    -  buscarPeloId;
    - alterarFicha;
    - excluirFicha;
    
    Modificação no metodo alterar, na classe FichaServiceImpl;

--- salutar-backend/src/main/java/br/com/isiflix/salutar/controller/FichaCrontroller.java
+++ salutar-backend/src/main/java/br/com/isiflix/salutar/controller/FichaCrontroller.java
@@ -1,11 +1,18 @@
 package br.com.isiflix.salutar.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +34,46 @@ public class FichaCrontroller {
 			}
 			return ResponseEntity.notFound().build();
 		}
+	
+	@PostMapping("/fichas")
+	public ResponseEntity<FichaPaciente> cadastrarNovaFicha(@RequestBody FichaPaciente nova) throws URISyntaxException{
+		FichaPaciente res = service.cadastrar(nova);
+		if(res != null) {
+			return ResponseEntity.created(new URI("/fichas/"+res.getIdFicha())).body(res);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@GetMapping("/fichas/{id}")
+	public ResponseEntity<FichaPaciente> buscarPeloId(@PathVariable Integer id){
+		FichaPaciente res = service.recuperarPeloId(id);
+		if(res != null) {
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/fichas/{id}")
+	public ResponseEntity<FichaPaciente> alterarFicha(@RequestBody FichaPaciente ficha, @PathVariable Integer id){
+		if(ficha.getIdFicha() == null) {
+			ficha.setIdFicha(id);
+		}
+		FichaPaciente res = service.alterar(ficha);
+		if(res != null) {
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.badRequest().build();
+	}
+	
+	@DeleteMapping("/fichas/{id}")
+	public ResponseEntity<FichaPaciente> excluirFicha(@PathVariable Integer id){
+		boolean res = service.excluir(id);
+		if(res) {
+			return ResponseEntity.ok(service.recuperarPeloId(id));
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	
 }
+	
--- salutar-backend/src/main/resources/import.sql
+++ salutar-backend/src/main/resources/import.sql
@@ -1,4 +1,3 @@
-insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (1, 'Isidro Francisco', '1978-10-04', 'M', 'aaa');
 insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (2, 'Jefferson Pedro', '1993-05-25', 'M', 'bbb');
 insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (3, 'Alessandra', '1994-01-01', 'F', 'ccc');
 insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (4, 'Maristela', '1990-01-01', 'F', 'ddd');
@@ -6,3 +5,4 @@ insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (5, 
 insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (6, 'Anna Thays', '1994-05-03', 'F', 'fff');
 insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (7, 'Brandão', '1998-01-01', 'M', 'ggg');
 insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (8, 'Vanessa', '1995-01-01', 'F', 'hhh');
+insert into tbl_fichapaciente (id_ficha, nome, data_nasc,sexo, uuid) values (9, 'Isidro Francisco', '1978-10-04', 'M', 'aaa');
